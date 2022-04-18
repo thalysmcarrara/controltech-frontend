a7471dd14734ae88f8af7c1045a92a28f669f1a1
thalysmcarrara <thalysmatiascarrara@gmail.com>
2022-04-18 18:12:46 -0400

    configure eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import * as React from "react"
 import {
   ChakraProvider,
-} from "@chakra-ui/react"
+} from '@chakra-ui/react';
+import * as React from 'react';
 
-import { theme } from './styles/theme'
+import { theme } from './styles/theme';
 
-export const App = () => (
-  <ChakraProvider theme={theme} >
-    <h1>Hello world</h1>
-  </ChakraProvider>
-)
+export function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <h1>Hello world</h1>
+    </ChakraProvider>
+  );
+}

a0e08fe6a08ee4a7713ea61998993af5dd0e7a20
thalysmcarrara <thalysmatiascarrara@gmail.com>
2022-04-23 18:16:34 -0400

    feat(dashboard): dashboard layout

---
+++ src/layouts/Dashboard.tsx
@@ -0,0 +1,31 @@
+import { Box, Flex } from '@chakra-ui/react';
+import { ReactNode } from 'react';
+
+import Header from '../components/Header';
+import Sidebar from '../components/Sidebar';
+
+interface DashboardProps {
+  children: ReactNode
+}
+
+export default function Dashboard({ children }: DashboardProps) {
+  return (
+    <Flex flexDirection="column" h="100vh">
+      <Header />
+      <Flex
+        w="100%"
+        my="8"
+        maxWidth={1480}
+        mx="auto"
+        px="6"
+        alignItems="baseline"
+        justifyContent="space-between"
+      >
+        <Sidebar />
+        <Box w="1140px" minH="500px" bg="gray.800" rounded="16">
+          {children}
+        </Box>
+      </Flex>
+    </Flex>
+  );
+}

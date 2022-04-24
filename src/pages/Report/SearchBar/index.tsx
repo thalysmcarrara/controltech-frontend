ab2555f48730d094a60dc0287e99986194f59f37
thalysmcarrara <thalysmatiascarrara@gmail.com>
2022-04-24 15:55:04 -0400

    feat: searchBar component

---
+++ src/pages/Report/SearchBar/index.tsx
@@ -0,0 +1,41 @@
+import {
+  Flex, FormControl, FormLabel, Icon, Input, Button,
+} from '@chakra-ui/react';
+import { RiSearchLine } from 'react-icons/ri';
+
+export default function SearchBar() {
+  return (
+    <Flex mx="auto">
+      <Flex>
+        <FormControl maxW="300px">
+          <FormLabel
+            htmlFor="search-report"
+            display="flex"
+            alignItems="center"
+            bg="gray.900"
+            border="1px"
+            borderColor="gray.400"
+            px="2"
+            rounded="6"
+          >
+            <Icon as={RiSearchLine} fontSize="20" />
+            <Input
+              placeholder="Digite o cnpj do cliente"
+              id="search-report"
+              type="text"
+              bg="gray.900"
+              variant="filled"
+              _hover={{
+                bg:
+              'gray.900',
+              }}
+              _focus={{ outline: 'none' }}
+              autoComplete="off"
+            />
+          </FormLabel>
+        </FormControl>
+        <Button colorScheme="facebook">Buscar</Button>
+      </Flex>
+    </Flex>
+  );
+}
